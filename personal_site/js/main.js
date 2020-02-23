be14c806cc04fc7f5ec13abf40b20d684f860005
Robert Vaughan <robertjwvaughan@gmail.com>
2020-02-23 17:46:45 +0000

    Changing the tetris layout and rendering the cells

--- personal_site/js/main.js
+++ personal_site/js/main.js
@@ -5,8 +5,11 @@
 	Simples
 */
 
+const ns = "http://www.w3.org/2000/svg";
+
 window.onload = () => {
 	const keys = ['a', 'w', 'd', 's'];
+	const space = 'spacebar';
 
 	document.getElementById("about-btn")
 		.addEventListener("click", _ => {
@@ -19,32 +22,81 @@ window.onload = () => {
 			goHome(e);
 		});
 
-	document.getElementById("tetris-home-btn")
-		.addEventListener("click", e => {
-			goHome(e);
-		});
+	// document.getElementById("tetris-home-btn")
+	// 	.addEventListener("click", e => {
+	// 		goHome(e);
+	// 	});
 
 	document.getElementById("tetris-btn")
 		.addEventListener("click", _ => {
 			document.getElementById("tetris").classList.toggle("active");
+			document.getElementById("spacebar-key").focus();
 			document.getElementById("home").classList.toggle("inactive");
+
+			renderTetrisBoard();
 		});
 
 	window.addEventListener("keydown", function(e) {
 		if (keys.includes(e.key.toLowerCase())) {
-			changeKeyColor(e.key.toLowerCase(), "#FFFFFF");
+			changeKeyColor(e.key.toLowerCase(), "#FFFFFF", "#000000");
+		}
+		else {
+			changeKeyColor(space, "#FFFFFF", "#000000");
 		}
 	});
 
 	window.addEventListener("keyup", function(e){
 		if (keys.includes(e.key.toLowerCase())) {
-			changeKeyColor(e.key.toLowerCase(), "#000000");
+			changeKeyColor(e.key.toLowerCase(), "#000000", "#FFFFFF");
+		}
+		else {
+			changeKeyColor(space, "#000000", "#FFFFFF");
 		}
 	});
 }
 
-function changeKeyColor(key, color) {
+function renderTetrisBoard() {
+	// Vanilla js
+
+	const boardLength = 20;
+	const boardWidth = 10;
+
+	const squareLength = Math.floor((document.getElementById("tetris-svg").clientWidth) / 10);
+
+	squarePixel = squareLength + "px";
+
+	const rootDiv = document.getElementById("tetris-svg");
+
+	rootDiv.style.width = (squareLength * (boardWidth)) + "px";
+	rootDiv.style.height = (squareLength * (boardLength)) + "px";
+
+	for (let i = 0; i < boardLength; i++) {
+		for (let j = 0; j < boardWidth; j++) {
+			const newCell = document.createElement("div");
+			
+			newCell.classList.add("tetris-cell");
+			newCell.style.width = squarePixel;
+			newCell.style.height = squarePixel;
+			// add the text node to the newly created div
+			rootDiv.appendChild(newCell);
+		}
+	}
+
+	// var div = document.getElementById('tetris-svg') 
+	// var svg = document.createElementNS(ns, 'svg')
+	// svg.setAttributeNS(null, 'width', squareLength)
+	// svg.setAttributeNS(null, 'height', squareLength)
+	// div.appendChild(svg)
+	// var rect = document.createElementNS(ns, 'rect')
+	// rect.setAttributeNS(null, 'width', 100)
+	// rect.setAttributeNS(null, 'height', 100)
+	// rect.setAttributeNS(null, 'fill', '#f06')
+	// svg.appendChild(rect)
+}
+
+function changeKeyColor(key, color, textColor) {
 	document.getElementById(key + "-key").style.background = color;
+	document.getElementById(key + "-key").style.color = textColor;
 }
 
 function goHome(event) {
